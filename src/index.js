e24f23703a544581502e986cb0eeef4e6a4922e9
ducnm46 <nduc04@gmail.com>
2022-07-03 21:40:25 +0700

    change origin

--- src/index.js
+++ src/index.js
@@ -26,7 +26,7 @@ const server = http.listen(port, () => {
 
 const io = new Server(server,{
     cors:{
-        origin: 'http://localhost:3000'
+        origin: 'https://adagaki-aki.herokuapp.com'
     }
 })
 
